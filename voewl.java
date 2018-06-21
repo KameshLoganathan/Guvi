4035c2dca97a2e61a3f1931b434d25cf50ff2329
KameshLoganathan <40458020+KameshLoganathan@users.noreply.github.com>
2018-06-21 10:07:57 +0000

    Create voewl.java

---
+++ voewl.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+class vowel
+{
+public static void main(String x[])
+{
+Scanner sc=new Scanner(System.in);
+String s=sc.nextLine();
+for(int i=0;i<s.length();i++)
+{
+if(s.chatAt(i)=='a' || s.chatAt(i)=='e' ||s.chatAt(i)=='i' || s.chatAt(i)=='o' || s.chatAt(i)=='u')
+{
+System.out.print("Vowel");
+}
+else
+{
+SYstem.out.print("Not Vowel"):
+}
+}
+}
+}
