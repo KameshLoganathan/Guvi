eea6f7de63df60137b716f5e52968b9cf9d33cac
KameshLoganathan <40458020+KameshLoganathan@users.noreply.github.com>
2018-06-21 09:51:26 +0000

    Update and rename .gitignore to pro1.java

---
+++ .gitignore/pro1.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+class pro1
+{
+public static void main(String x[])
+{
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+if(n>0)
+{
+System.out.print("Positive");
+}
+else if(n<0)
+{
+System.out.print("Negative");
+}
+else if(n==0)
+{
+system.out.print("Zero");
+}
+}
+}
