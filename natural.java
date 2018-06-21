c4a61c6144e658a01e2826ceac6ef97481c6ae28
KameshLoganathan <40458020+KameshLoganathan@users.noreply.github.com>
2018-06-21 11:46:42 +0000

    Create natural.java

---
+++ natural.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+class natural
+{
+public static void main(String x[])
+{
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+int tut=n(n+1)/2;
+System.out.print(tut);
+}
+}
