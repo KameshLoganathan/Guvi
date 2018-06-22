06c5e93efdf7457949414a4bfe312d768be3859a
KameshLoganathan <40458020+KameshLoganathan@users.noreply.github.com>
2018-06-22 15:55:56 +0530

    Create aeesort.java

---
+++ aeesort.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+class arraymax
+{
+public static void main(String x[])
+{
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+int m=0;
+int a[]=new int[n];
+for(int i=0;i<n;i++)
+{
+a[i]=sc.nextInt();
+}
+Arrays.sort(a);
+System.out.print(a);
+}
+}
