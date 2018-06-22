6e05f282e62928e473e7698ef35bd90d33037c85
KameshLoganathan <40458020+KameshLoganathan@users.noreply.github.com>
2018-06-22 15:15:28 +0530

    Create inprime.java

---
+++ inprime.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+class prime
+{
+public static void main(String x[])
+{
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+int n1=sc.nextInt();
+for(i=n;i<=n1;i++)
+{
+if(n==1 || n==2 ||n==3||n==5||n==7)
+{
+System.out.print(i);
+}
+else if(n%2!=0&&n%2!=0&&n%2!=0&&n%2!=0)
+{
+System.out.print(i);
+}
+}
+}
+}
