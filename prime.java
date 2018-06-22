0b1246bcdac4e0e385f4551aa8aee0cebe5818cc
KameshLoganathan <40458020+KameshLoganathan@users.noreply.github.com>
2018-06-22 15:03:04 +0530

    Create prime.java

---
+++ prime.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+class prime
+{
+public static void main(String x[])
+{
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+if(n==1 || n==2 ||n==3||n==5||n==7)
+{
+System.out.print("Yes");
+}
+else if(n%2!=0&&n%2!=0&&n%2!=0&&n%2!=0)
+{
+System.out.print("No");
+}
+}
+}
