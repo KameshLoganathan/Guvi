cc6b271e85af8f265659368df15b4292276b3564
KameshLoganathan <40458020+KameshLoganathan@users.noreply.github.com>
2018-06-22 15:31:39 +0530

    Create fact.java

---
+++ fact.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+class fact
+{
+public static void main(String x[])
+{
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+for(int i=n;i>0;i--)
+{
+n=n*(n-1);
+}
+System.out.print(n):
+}
+}
