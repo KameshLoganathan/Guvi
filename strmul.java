1f9f67cc6e19cf27749dca609cfe34396e0bac96
KameshLoganathan <40458020+KameshLoganathan@users.noreply.github.com>
2018-06-22 15:38:49 +0530

    Create strmul.java

---
+++ strmul.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+class strmul
+{
+public static void main(String x[])
+{
+Scanner sc=new Scanner(System.in);
+String s=sc.next();
+for(int i=0;i<s.length();i++)
+{
+System.out.print((i)*(i+1));
+}
+}
+}
