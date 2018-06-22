2ae77b9d517fea1c927fd3195c1cf8004c211d5b
KameshLoganathan <40458020+KameshLoganathan@users.noreply.github.com>
2018-06-22 15:11:09 +0530

    Create ineven.java

---
+++ ineven.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+class inodd
+{
+public static void main(String x[])
+{
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+int n1=sc.nextInt();
+for(int i=n;i<=n1;i++)
+{
+if(i%2==0)
+System.out.print(i);
+else
+System.out.print(i);
+}
+}
+}
