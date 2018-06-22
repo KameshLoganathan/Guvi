22583dc479e1cf8db8691b0e0fa385247c013aa4
KameshLoganathan <40458020+KameshLoganathan@users.noreply.github.com>
2018-06-22 15:34:31 +0530

    Create mul.java

---
+++ mul.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+class mul
+{
+public static void main(String x[])
+{
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+for(int i=1;i<=n;i++)
+{
+System.out.print(i*n);
+}
+}
+}
