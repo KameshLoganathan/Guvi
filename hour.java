45328b5dc25d92393025f2ab8b33254c2409428c
KameshLoganathan <40458020+KameshLoganathan@users.noreply.github.com>
2018-06-23 08:41:33 +0000

    Create hour.java

---
+++ hour.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+class hour
+{
+public staic void main(String x[])
+{
+Scanner sc=new Scanner(System.in);
+int t=sc.nextInt();
+int hours=t/60;
+int minutes=t%60;
+System.out.print(hours+""+minutes);
+}
+}
