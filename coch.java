52d33c7dbefa7a4d267cf3d9e4c743a8baf0bc07
KameshLoganathan <40458020+KameshLoganathan@users.noreply.github.com>
2018-06-25 14:19:36 +0530

    Create coch.java

---
+++ coch.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+class coch
+{
+public static void main(String x[])
+{
+Scanner sc=new Scanner(System.in);
+String s=sc.next();
+System.out.print(s.length());
+}
+}
