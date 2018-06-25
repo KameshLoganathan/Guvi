a232104c863c0be557b6127d53b935e7940a8858
KameshLoganathan <40458020+KameshLoganathan@users.noreply.github.com>
2018-06-25 14:25:44 +0530

    Create cowo.java

---
+++ cowo.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+class cowo
+{
+public static void main(String x[])
+{
+Scanner sc=new Scanner(System.in);
+String s=sc.nextLine();
+String s1[]=s.split("");
+System.out.print(s1.length());
+}
+}
