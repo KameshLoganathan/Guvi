2bde8bdef87b0b557b032cd8a6ad6624e18493e4
KameshLoganathan <40458020+KameshLoganathan@users.noreply.github.com>
2018-06-25 14:51:36 +0530

    Create dinu.java

---
+++ dinu.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+class cowo
+{
+public static void main(String x[])
+{
+Scanner sc=new Scanner(System.in);
+int count=0;
+String s1=sc.next();
+for(int i=0;i<s.length();i++)
+{
+if(Character.isDgit(s1.chatAt(i)))
+{
+System.out.print(count++);
+}
+}
+}
