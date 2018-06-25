72366f25b9d61b729b8a4ccb70fcac1dc1e8cb77
KameshLoganathan <40458020+KameshLoganathan@users.noreply.github.com>
2018-06-25 14:48:31 +0530

    Create conu.java

---
+++ conu.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+class cowo
+{
+public static void main(String x[])
+{
+Scanner sc=new Scanner(System.in);
+int count=0;
+String s1[]=s.split("\r\n|\r|\n");
+System.out.print(count++);
+}
+}
