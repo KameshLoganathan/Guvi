d2bf66ff55f6b976ba6fb00e5d64382c12315462
KameshLoganathan <40458020+KameshLoganathan@users.noreply.github.com>
2018-07-07 12:02:49 +0530

    Rename set43.java to set44.java

---
+++ set44.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+class swap
+{
+public static void main(String x[])
+{
+Scanner sc=new Scannner(System.in);
+int n=sc.nextInt();
+for(int i=0;i<=10;i++)
+{
+if(n==i)
+System.out.print("YES");
+else
+System.out.print("NO");
+}
+}
+}
