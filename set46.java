4568d519b7d883920c19e033e1f986b4a02ab616
KameshLoganathan <40458020+KameshLoganathan@users.noreply.github.com>
2018-07-07 12:10:09 +0530

    Create set46.java

---
+++ set46.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+class swap
+{
+public static void main(String x[])
+{
+Scanner sc=new Scannner(System.in);
+int n=sc.nextInt();
+int a=n+1;
+System.out.print(a);
+}
+}
