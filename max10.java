f9549c30c6fe732f4ce2e4b71e73c1d22c988715
KameshLoganathan <40458020+KameshLoganathan@users.noreply.github.com>
2018-07-07 11:40:58 +0530

    Create max10.java

---
+++ max10.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+class swap
+{
+public static void main(String x[])
+{
+Scanner sc=new Scannner(System.in);
+int a=10;
+int max=-10;
+int n[]=new int[a];
+for(int i=0;i<10;i++)
+{
+if(a[i]<min)
+{
+min=a[i];
+}
+}
+System.out.print(min);
+}
+}
