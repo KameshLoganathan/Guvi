3197d3f20ebdce7a2cd4b9726a25e8ed14e2491b
KameshLoganathan <40458020+KameshLoganathan@users.noreply.github.com>
2018-07-07 11:52:34 +0530

    Create set41.java

---
+++ set41.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+class swap
+{
+public static void main(String x[])
+{
+Scanner sc=new Scannner(System.in);
+String s=sc.next();
+int n=sc.nextInt();
+for(int i=0;i<n;i++)
+{
+System.out.print(s);
+}
+}
+}
