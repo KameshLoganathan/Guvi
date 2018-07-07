c3482c3c79b254ea500e13a60498b9eb1422aa4e
KameshLoganathan <40458020+KameshLoganathan@users.noreply.github.com>
2018-07-07 11:59:55 +0530

    Create set43.java

---
+++ set43.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+class swap
+{
+public static void main(String x[])
+{
+Scanner sc=new Scannner(System.in);
+String s=sc.next();
+String s1=sc.next();
+System.out.print(s+s1);
+}
+}
