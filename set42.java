756b6263ed9e9b70e610cb640770332ffa9e4b3a
KameshLoganathan <40458020+KameshLoganathan@users.noreply.github.com>
2018-07-07 11:58:01 +0530

    Create set42.java

---
+++ set42.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+class swap
+{
+public static void main(String x[])
+{
+Scanner sc=new Scannner(System.in);
+String s=sc.next();
+String s1=sc.next();
+if(s.equals(s1))
+{
+if(s.length()>=s1.length())
+{
+System.out.print(s);
+}
+else
+{
+System.out.print(s1);
+}
+}
+}
+}
