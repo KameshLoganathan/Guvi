2b22f98b8aed85cc84391ac5164492bad0c1d833
KameshLoganathan <40458020+KameshLoganathan@users.noreply.github.com>
2018-07-07 11:34:45 +0530

    Create swap.java

---
+++ swap.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+class swap
+{
+public static void main(String x[])
+{
+Scanner sc=new Scannner(System.in);
+int a=sc.nextInt();
+int b=sc.nextInt();
+int c;
+c=a;
+a=b;
+b=c;
+System.out.print(a+""+b);
+}
+}
