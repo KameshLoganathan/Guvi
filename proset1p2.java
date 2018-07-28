aa495964189d62de9f9a8fdd8d853ef6480dd4e2
KameshLoganathan <40458020+KameshLoganathan@users.noreply.github.com>
2018-07-28 10:36:31 +0530

    Create proset1p2.java

---
+++ proset1p2.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import javautil.*;
+calss proset1p2
+{
+public static void main(String x[])
+{
+Scanner sc=new Scanner(System.in);
+String s=sc.next();
+String s1="";
+int n=sc.nextInt():
+for(int i=0;i<s.length();i++)
+{
+if(Character.getNumericValue(s.charAt(i))==n)
+s1=s.substring(i,s.length()-1);
+}
+}
+}
