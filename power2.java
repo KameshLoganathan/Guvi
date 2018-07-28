6c203fd6fcea815aa52a10c0c850e938a32fd05e
KameshLoganathan <40458020+KameshLoganathan@users.noreply.github.com>
2018-07-28 10:17:53 +0530

    Create power2.java

---
+++ power2.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+class power2
+{
+public static void main(String x[])
+{
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+if((2*2)==n)
+System.out.print("YES");
+else
+System.out.print("NO");
+}
+}
