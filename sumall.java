22909dd31965d9724f9e69ea6259f57db07ef288
KameshLoganathan <40458020+KameshLoganathan@users.noreply.github.com>
2018-07-28 10:13:23 +0530

    Create sumall.java

---
+++ sumall.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+class sumall
+{
+public static void main(String x[])
+{
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+int n1=sc.nextInt();
+int sum=n+n1;
+System.out.print(sum);
+}
+}
